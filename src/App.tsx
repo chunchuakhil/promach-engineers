b9ea6b3fbe2df021268706a4a6d759d9a223ca4f
akhil chunchu <akhilchunchu52@gmail.com>
2023-07-05 17:55:47 +0530

    added appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import { MantineProvider, Text } from '@mantine/core';
+import { HeaderResponsive } from './components/AppBar';
+interface HeaderResponsiveProps {
+  links: { link: string; label: string }[];
+}
 
+const AppBarLinks: HeaderResponsiveProps = {
+  links: [
+    {
+      link: 'https://mantine.dev',
+      label: 'Mantine',
+    },
+    {
+      link: 'https://mantine.dev/docs',
+      label: 'Docs',
+    },
+    {
+      link: 'https://github.com/mantine/mantine',
+      label: 'GitHub',
+    },
+  ],
+}
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text>Welcome to Mantine!</Text>
+      <HeaderResponsive links={AppBarLinks.links}/>
     </MantineProvider>
   );
 }
